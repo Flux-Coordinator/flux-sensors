b895ea5d2a79ad9bf7cf558dcb308c1eac21e748
Patrick Scherler <pacs01dev@gmail.com>
2018-04-16 15:00:38 +0200

    refactor code and add exception handling

--- flux_sensors/__main__.py
+++ flux_sensors/__main__.py
@@ -1,26 +1,29 @@
 from flux_sensors.localizer import localizer
+from flux_sensors.light_sensor import light_sensor
 
 
 def main():
     """entry point"""
-    run_pozyx()
+    initialize_sensors()
+    start_measurement()
 
 
-def run_pozyx():
-    serial_port = localizer.get_first_pozyx_serial_port()
-    if serial_port is None:
-        print("No Pozyx connected. Check your USB cable or your driver!")
-        quit()
+pozyx_localizer = None
 
-    pozyx = localizer.PozyxSerial(serial_port)
+
+def initialize_sensors():
+    pozyx = localizer.Localizer.get_device()
     pozyx_localizer = localizer.Localizer(pozyx)
 
-    pozyx_localizer.add_anchor(0x6e4e, localizer.Coordinates(-100, 100, 1150))
-    pozyx_localizer.add_anchor(0x6964, localizer.Coordinates(8450, 1200, 2150))
-    pozyx_localizer.add_anchor(0x6e5f, localizer.Coordinates(1250, 12000, 1150))
-    pozyx_localizer.add_anchor(0x6e62, localizer.Coordinates(7350, 11660, 1590))
+    pozyx_localizer.add_anchor_to_cache(0x6e4e, localizer.Coordinates(-100, 100, 1150))
+    pozyx_localizer.add_anchor_to_cache(0x6964, localizer.Coordinates(8450, 1200, 2150))
+    pozyx_localizer.add_anchor_to_cache(0x6e5f, localizer.Coordinates(1250, 12000, 1150))
+    pozyx_localizer.add_anchor_to_cache(0x6e62, localizer.Coordinates(7350, 11660, 1590))
 
     pozyx_localizer.initialize()
+
+
+def start_measurement():
     while True:
         pozyx_localizer.do_positioning()
 
