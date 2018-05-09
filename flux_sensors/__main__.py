a3e7ce99ab34bb17548b4fd1a987d33fe89b195b
Patrick Scherler <pacs01dev@gmail.com>
2018-05-09 16:08:23 +0200

    add config file parser and support for multiple urls

--- flux_sensors/__main__.py
+++ flux_sensors/__main__.py
@@ -1,13 +1,12 @@
 from flux_sensors.localizer.localizer import Localizer, Coordinates
 from flux_sensors.light_sensor.light_sensor import LightSensor
 from flux_sensors.models import models
-from http.client import responses
-import polling
+from flux_sensors.config_loader import ConfigLoader
+from flux_sensors.server_probe import ServerProbe
 import requests
 import json
 
 AMS_LIGHT_SENSOR_I2C_ADDRESS = 0x39
-SERVER_URL = "http://localhost:9000"
 
 
 def main() -> None:
@@ -18,57 +17,41 @@ def main() -> None:
     ams_device = LightSensor.get_device(1)
     ams_light_sensor = LightSensor(AMS_LIGHT_SENSOR_I2C_ADDRESS, ams_device)
 
-    flux_sensor = FluxSensor(pozyx_localizer, ams_light_sensor, SERVER_URL)
-    print("Flux-sensors available.")
-    while True:
-        print("Start measurement when server ready...")
-        flux_sensor.wait_for_server_readiness()
-        print("Success! A flux-server is available and a measurement is active.")
-        flux_sensor.initialize_sensors()
-        flux_sensor.start_measurement()
-        flux_sensor.clear_sensors()
+    config_loader = ConfigLoader()
+
+    flux_server = ServerProbe()
+
+    flux_sensor = FluxSensor(pozyx_localizer, ams_light_sensor, config_loader, flux_server)
+    flux_sensor.start_when_ready()
 
 
-class FluxSensor(object):
+class FluxSensor:
     """Controlling class for the flux-sensors components"""
 
-    CHECK_ACTIVE_MEASUREMENT_ROUTE = "/measurements/active"
-    ADD_READINGS_ROUTE = "/measurements/active/readings"
-
-    def __init__(self, localizer_instance: Localizer, light_sensor_instance: LightSensor, server_url: str) -> None:
+    def __init__(self, localizer_instance: Localizer, light_sensor_instance: LightSensor, config_loader: ConfigLoader,
+                 flux_server: ServerProbe) -> None:
         self._localizer = localizer_instance
         self._light_sensor = light_sensor_instance
-        self._server_url = server_url
-        self._check_ready_counter = 0
+        self._config_loader = config_loader
+        self._flux_server = flux_server
 
-    def wait_for_server_readiness(self) -> None:
-        self._check_ready_counter = 0
-        polling.poll(
-            target=lambda: requests.get(self._server_url + self.CHECK_ACTIVE_MEASUREMENT_ROUTE),
-            check_success=self.check_polling_success,
-            step=3,
-            step_function=self.log_polling_step,
-            ignore_exceptions=(requests.exceptions.RequestException,),
-            poll_forever=True)
+    def start_when_ready(self) -> None:
+        print("Flux-sensors in standby. Start polling Flux-server")
+        while True:
+            if not self._flux_server.poll_server_urls(self._config_loader.get_server_urls(),
+                                                      self._config_loader.get_timeout()):
+                print("All server URLs failed to respond. Retry started...")
+                continue
+            print("Server responding. Start measurement when ready...")
 
-    def check_polling_success(self, response: requests.Response) -> bool:
-        self.log_server_response(response)
-        return response.status_code == 200
+            if not self._flux_server.poll_active_measurement():
+                continue
+            print("Success! A flux-server is available and a measurement is active.")
 
-    def log_polling_step(self, step: int) -> int:
-        self._check_ready_counter += 1
-        print("Polling Flux-server at {}: {} attempt(s)".format(self._server_url + self.CHECK_ACTIVE_MEASUREMENT_ROUTE,
-                                                                self._check_ready_counter))
-        return step
-
-    def log_server_response(self, response: requests.Response) -> None:
-        description = ""
-        if response.status_code == 204:
-            description = " -> no active measurement available"
-        elif response.status_code == 400:
-            description = " -> check firewall settings or AllowedHostsFilter from flux-server"
-        print("Response: {} ({}){}".format(response.status_code, responses[response.status_code],
-                                           description))
+            self.initialize_sensors()
+            print("Flux-sensors initialized. Start measurement...")
+            self.start_measurement()
+            self.clear_sensors()
 
     def initialize_sensors(self) -> None:
         self.initialize_localizer()
@@ -98,8 +81,8 @@ class FluxSensor(object):
             json_data = json.dumps(readings, default=lambda o: o.__dict__)
 
             try:
-                response = self.send_data_to_server(json_data)
-                self.log_server_response(response)
+                response = self._flux_server.send_data_to_server(json_data)
+                ServerProbe.log_server_response(response)
 
                 if response.status_code == 204:
                     print("The measurement has been stopped by the server.")
@@ -110,11 +93,6 @@ class FluxSensor(object):
             except requests.exceptions.RequestException as err:
                 print(err)
                 return
-
-    def send_data_to_server(self, json_data: str) -> requests.Response:
-        print("Sending: {}".format(json_data))
-        headers = {'content-type': 'application/json'}
-        return requests.post(self._server_url + self.ADD_READINGS_ROUTE, data=json_data, headers=headers)
 
 
 if __name__ == "__main__":
