4b77a4e153bb8838f05ad0cfd50c5124f5e65839
Patrick Scherler <pacs01dev@gmail.com>
2018-06-02 14:53:32 +0200

    handle pozyx device errors

--- flux_sensors/flux_sensor.py
+++ flux_sensors/flux_sensor.py
@@ -1,4 +1,4 @@
-from flux_sensors.localizer.localizer import Localizer, Coordinates
+from flux_sensors.localizer.localizer import Localizer, Coordinates, PozyxDeviceError
 from flux_sensors.light_sensor.light_sensor import LightSensor
 from flux_sensors.config_loader import ConfigLoader
 from flux_sensors.flux_server import FluxServer
@@ -95,24 +95,28 @@ class FluxSensor:
         readings = []
         self._flux_server.initialize_last_response()
         while True:
-            position = self._localizer.do_positioning()
-            illuminance = self._light_sensor.do_measurement()
-
-            readings.append(models.Reading(illuminance, position))
-
             try:
-                if self._flux_server.get_last_response() == 200:
-                    if len(readings) >= self._flux_server.MIN_BATCH_SIZE:
-                        self._flux_server.reset_last_response()
-                        json_data = json.dumps(readings, default=lambda o: o.__dict__)
-                        self._flux_server.send_data_to_server(json_data)
-                        del readings[:]
-                elif self._flux_server.get_last_response() == 404:
-                    logger.info("The measurement has been stopped by the server.")
+                position = self._localizer.do_positioning()
+                illuminance = self._light_sensor.do_measurement()
+
+                readings.append(models.Reading(illuminance, position))
+
+                try:
+                    if self._flux_server.get_last_response() == 200:
+                        if len(readings) >= self._flux_server.MIN_BATCH_SIZE:
+                            self._flux_server.reset_last_response()
+                            json_data = json.dumps(readings, default=lambda o: o.__dict__)
+                            self._flux_server.send_data_to_server(json_data)
+                            del readings[:]
+                    elif self._flux_server.get_last_response() == 404:
+                        logger.info("The measurement has been stopped by the server.")
+                        return
+                    elif self._flux_server.get_last_response() != self._flux_server.RESPONSE_PENDING:
+                        logger.info("The measurement has been stopped.")
+                        return
+                except requests.exceptions.RequestException as err:
+                    logger.error(err)
                     return
-                elif self._flux_server.get_last_response() != self._flux_server.RESPONSE_PENDING:
-                    logger.info("The measurement has been stopped.")
-                    return
-            except requests.exceptions.RequestException as err:
+            except PozyxDeviceError as err:
                 logger.error(err)
-                return
+                continue
